741523316c9eec849d6e2b4d15906fc9d4b0d3b7
ahmed <alaidy.y123@gmail.com>
2023-11-20 19:25:37 +0200

    color update

--- src/Modules/Product/ProductSelection/ProductSelectionColor.js
+++ src/Modules/Product/ProductSelection/ProductSelectionColor.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ButtonColor from 'Components/Buttons/ButtonColor';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import ButtonColor from "Components/Buttons/ButtonColor";
+import { Button } from "react-bootstrap";
 
-
-const ProductSelectionSize = ({color, setColor }) => {
-  console.log('color is ',color)
+const ProductSelectionSize = ({ color, setColor }) => {
+  console.log("color is ", color);
   return (
     <div className="mt-3">
-      <h4 style={{ marginBottom: "5px" }}>Color</h4>
+      <h4>Color</h4>
+      <div
+        className="d-flex justify-content-center mt-2"
+        style={{ marginBottom: "5px" }}
+      >
+        <ButtonColor name="red" background="red" onClick={setColor} />
+        <ButtonColor name="yellow" background="yellow" onClick={setColor} />
+        <ButtonColor name="blue" background="blue" onClick={setColor} />
+        <ButtonColor name="black" background="black" onClick={setColor} />
+        <ButtonColor name="grey" background="grey" onClick={setColor} />
+      </div>
       <Button
         style={{
           backgroundColor: color,
@@ -17,13 +26,6 @@ const ProductSelectionSize = ({color, setColor }) => {
         }}
         className="rounded-circle"
       />
-      <div className="d-flex justify-content-center mt-2">
-        <ButtonColor name="red" background="red" onClick={setColor} />
-        <ButtonColor name="yellow" background="yellow" onClick={setColor} />
-        <ButtonColor name="blue" background="blue" onClick={setColor} />
-        <ButtonColor name="black" background="black" onClick={setColor} />
-        <ButtonColor name="grey" background="grey" onClick={setColor} />
-      </div>
     </div>
   );
 };
